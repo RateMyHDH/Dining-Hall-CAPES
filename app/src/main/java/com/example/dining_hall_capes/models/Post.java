342a79a157e10573c4e35296349cfb1815c16629
Tim-404 <tzw202623@yahoo.com>
2021-11-03 20:05:18 -0700

    Basic data models

--- app/src/main/java/com/example/dining_hall_capes/models/Post.java
+++ app/src/main/java/com/example/dining_hall_capes/models/Post.java
@@ -1,4 +1,38 @@
 package com.example.dining_hall_capes.models;
 
-public class Post {
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_LIKES = "likes";
+
+    public ParseUser getAuthor() {
+        return getParseUser(KEY_AUTHOR);
+    }
+
+    public void setAuthor(ParseUser author) {
+        put(KEY_AUTHOR, author);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image) {
+        put(KEY_IMAGE, image);
+    }
+
+    public int getLikes() {
+        return getInt(KEY_LIKES);
+    }
+
+    public void setLikes(int likes) {
+        put(KEY_LIKES, likes);
+    }
 }
--- app/src/main/java/com/example/dining_hall_capes/models/Vendor.java
+++ app/src/main/java/com/example/dining_hall_capes/models/Vendor.java
@@ -1,4 +1,29 @@
 package com.example.dining_hall_capes.models;
 
-public class Vendor {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.List;
+
+@ParseClassName("Vendor")
+public class Vendor extends ParseObject {
+
+    public static final String KEY_DISCUSSION_THREAD = "discussionThread";
+    public static final String KEY_RATING = "rating";
+
+    public List<Post> getDiscussionThread() {
+        return getList(KEY_DISCUSSION_THREAD);
+    }
+
+    public void setDiscussionThread(List<Post> discussionThread) {
+        put(KEY_DISCUSSION_THREAD, discussionThread);
+    }
+
+    public double getRating() {
+        return getDouble(KEY_RATING);
+    }
+
+    public void setRating(double rating) {
+        put(KEY_RATING, rating);
+    }
 }
