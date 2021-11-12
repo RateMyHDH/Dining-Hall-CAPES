02ae17b1ff74e4bd472a0d9714e73e6ae3c5caf6
Ryan <harsonoryan@gmail.com>
2021-11-12 15:12:04 -0800

    Initialized screen for fragment_creation.xml and backend CreationFragment Java class, also added a getter and setter for the Review text box in Post Java class

--- app/src/main/java/com/example/dining_hall_capes/models/Post.java
+++ app/src/main/java/com/example/dining_hall_capes/models/Post.java
@@ -12,6 +12,7 @@ public class Post extends ParseObject {
     public static final String KEY_IMAGE = "image";
     public static final String KEY_LIKES = "likes";
     public static final String KEY_VENDOR = "vendor";
+    public static final String KEY_REVIEW = "review";
 
     public ParseUser getAuthor() {
         return getParseUser(KEY_AUTHOR);
@@ -19,6 +20,14 @@ public class Post extends ParseObject {
 
     public void setAuthor(ParseUser author) {
         put(KEY_AUTHOR, author);
+    }
+
+    public String getReview(){
+        return getString(KEY_REVIEW);
+    }
+
+    public void setReview(String review){
+        put(KEY_REVIEW, review);
     }
 
     public ParseFile getImage() {
