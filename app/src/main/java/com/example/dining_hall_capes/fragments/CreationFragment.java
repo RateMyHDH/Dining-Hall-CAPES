cc9502208c13fdbf1e172d5a8ade497e83a42af8
Alejandro Marquez <50189645+Alex-942@users.noreply.github.com>
2021-11-19 18:23:33 -0800

    Revert "Dev"

--- app/src/main/java/com/example/dining_hall_capes/fragments/CreationFragment.java
+++ app/src/main/java/com/example/dining_hall_capes/fragments/CreationFragment.java
@@ -1,129 +1,66 @@
 package com.example.dining_hall_capes.fragments;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.Toast;
 
 import com.example.dining_hall_capes.R;
-import com.example.dining_hall_capes.models.Post;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.File;
-
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class CreationFragment extends Fragment {
 
-    public static final String TAG = "ComposeFragment";
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42; // arbitrary
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-    private EditText etReview;
-    private Button btnCaptureImage;
-    private ImageView ivPostImage;
-    private Button btnSubmit;
-    private File photoFile;
-    private String photoFileName = "photo.jpg";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
-    public CreationFragment(){
+    public CreationFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CreationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CreationFragment newInstance(String param1, String param2) {
+        CreationFragment fragment = new CreationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_creation, container, false);
     }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        etReview = view.findViewById(R.id.etReview);
-        btnCaptureImage = view.findViewById(R.id.btnCaptureImage);
-        ivPostImage = view.findViewById(R.id.ivPostImage);
-        btnSubmit = view.findViewById(R.id.btnSubmit);
-
-        btnCaptureImage.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                launchCamera();
-            }
-        });
-
-        btnSubmit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                String review = etReview.getText().toString();
-                if (review.isEmpty()) {
-                    Toast.makeText(getContext(), "Review cannot be empty", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(review, currentUser, photoFile);
-            }
-        });
-    }
-
-    private void launchCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        photoFile = getPhotoFileUri(photoFileName);
-
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        if(intent.resolveActivity(getContext().getPackageManager()) != null){
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-
-    }
-
-    private File getPhotoFileUri(String fileName) {
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        if(!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        return new File(mediaStorageDir.getPath() + File.separator + fileName);
-    }
-
-    private void savePost(String review, ParseUser currentUser, File photoFile) {
-        Post post = new Post();
-        post.setReview(review);
-        if(photoFile != null || ivPostImage.getDrawable() != null){
-            post.setImage(new ParseFile(photoFile));
-        }
-        post.setAuthor(currentUser);
-        post.saveInBackground(new SaveCallback(){
-            @Override
-            public void done(ParseException e) {
-                if(e != null){
-                    Log.e(TAG, "Error while saving", e);
-                    Toast.makeText(getContext(), "Error while saving", Toast.LENGTH_SHORT).show();
-                }
-                Log.i(TAG, "Post saved successfully");
-                etReview.setText("");
-                ivPostImage.setImageResource(0);
-            }
-        });
-    }
-
 }
