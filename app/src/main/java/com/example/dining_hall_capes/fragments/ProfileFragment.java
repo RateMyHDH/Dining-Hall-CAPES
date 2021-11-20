aab6d2f67923df50c795781261718797172ca365
Alex-942 <a1marque@ucsd.edu>
2021-11-19 18:20:10 -0800

    profile fragment with allow change PFP

--- app/src/main/java/com/example/dining_hall_capes/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/dining_hall_capes/fragments/ProfileFragment.java
@@ -1,12 +1,19 @@
 package com.example.dining_hall_capes.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +23,29 @@ import android.widget.Toast;
 
 import com.example.dining_hall_capes.LoginActivity;
 import com.example.dining_hall_capes.R;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public static final String TAG = "ProfileFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+
+    private ParseUser currentUser = ParseUser.getCurrentUser();
+
     private ImageView ivProfilePic;
     private Button btnSwapPic;
     private Button btnLogout;
+
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -46,13 +67,25 @@ public class ProfileFragment extends Fragment {
         btnSwapPic = view.findViewById(R.id.btnSwapPic);
         btnLogout = view.findViewById(R.id.btnLogout);
 
-        /*
-        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+        if(currentUser.getParseFile("profilePic") != null) {
+            (currentUser.getParseFile("profilePic")).getDataInBackground(new GetDataCallback() {
+                @Override
+                public void done(byte[] data, ParseException e) {
+                    if(e == null){
+                        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        ivProfilePic.setImageBitmap(bmp);
+                    }
+                }
+            });
+        }
+
+
+        btnSwapPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchCamera();
             }
-        });*/
+        });
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +101,71 @@ public class ProfileFragment extends Fragment {
         Intent i = new Intent(getContext(), LoginActivity.class);
         startActivity(i);
         getActivity().finish();
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider.pictures", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        // Start the image capture intent to take photo
+        // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivProfilePic.setImageBitmap(takenImage);
+                currentUser.put("profilePic", new ParseFile(photoFile));
+                currentUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e != null){
+                            Log.e(TAG, "Error while saving", e);
+                            Toast.makeText(getContext(), "PFP did not save :(", Toast.LENGTH_LONG).show();
+                        }
+                        Toast.makeText(getContext(), "PFP Saved Succeed", Toast.LENGTH_LONG).show();
+                        Log.i(TAG, "PFP save was successful");
+                    }
+                });
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
 }
